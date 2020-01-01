6badd6a2dbf1817d4fd4cb20f16333e90a4a4ea8
Kunal Pathak <Kunal.Pathak@microsoft.com>
2020-01-01 12:10:21 +0530

    common testimonial and added image

---
+++ common/testimonial.js
@@ -0,0 +1,30 @@
+document.write(`
+<div class="site-section section-4">
+      <div class="container">
+
+        <div class="row justify-content-center text-center">
+          <div class="col-md-7">
+            <div class="slide-one-item owl-carousel">
+              <blockquote class="testimonial-1">
+                <span class="quote quote-icon-wrap"><span class="icon-format_quote"></span></span>
+                <p>We love Priyanka's sense of interior design.</p>
+                <cite><span class="text-black">Ekam's mom</span> &mdash; <span class="text-muted">Software developer, Ebay</span></cite>
+              </blockquote>
+
+              <blockquote class="testimonial-1">
+                <span class="quote quote-icon-wrap"><span class="icon-format_quote"></span></span>
+                <p>Highly recommend Priyanka for interior design.</p>
+                <cite><span class="text-black">Dola's mom</span> &mdash; <span class="text-muted">Home maker</span></cite>
+              </blockquote>
+
+              <blockquote class="testimonial-1">
+                <span class="quote quote-icon-wrap"><span class="icon-format_quote"></span></span>
+                <p>Priyanka has good taste of colors.</p>
+                <cite><span class="text-black">Lori</span> &mdash; <span class="text-muted">Manager, Calico corners</span></cite>
+              </blockquote>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+`);
